08751611b6830663ac321e53343a553709f9140f
akshitkdesign <akshitrke920@gmail.com>
2025-09-20 15:43:13 +0000

    Add Day26 to Day31 folders

---
+++ Day31/Day31_Q62.c
@@ -0,0 +1,39 @@
+// #100DaysofCode
+// Q62. Write a program to reverse an array without taking extra space.
+//
+// Sample Test Cases:
+// Input 1:
+// 4
+// 1 2 3 4
+// Output 1:
+// 4 3 2 1
+
+#include <stdio.h>
+
+int main() {
+    int n, i, temp;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse in-place
+    for (i = 0; i < n/2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+
+    printf("Reversed array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
